df8a8c283313da23be44243edf8945d3dd8e78df
sjice80 <sjice80@gmail.com>
2019-08-02 16:20:30 +0900

    Update sub2.java

--- sub2.java
+++ sub2.java
@@ -4,9 +4,11 @@ import java.io.*;
 class Bus {
 	private String busId;
 	private int busLoc;
+	private int busVel;
 	public Bus(String line) {
 		this.busId = line.split(",")[0];
 		this.busLoc = Integer.parseInt(line.split(",")[1]);
+		this.busVel = 0;
 	}
 	public String getBusId() {
 		return this.busId;
@@ -14,20 +16,34 @@ class Bus {
 	public int getBusLoc() {
 		return this.busLoc;
 	}
+	public int getBusVel() {
+		return this.busVel;
+	}
+	public void setBusVel(int vel) {
+		this.busVel = vel;;
+	}
 }
 
 class Station {
 	private String statId;
 	private int statLoc;
+	private int statVel;
 	public Station(String line) {
 		this.statId = line.split("#")[0];
 		this.statLoc = Integer.parseInt(line.split("#")[1]);
+		this.statVel = Integer.parseInt(line.split("#")[2]);
 	}
 	public String getStatId() {
 		return this.statId;
 	}
 	public int getStatLoc() {
 		return this.statLoc;
+	}
+	public int getStatVel() {
+		return this.statVel;
+	}
+	public void setStatVel(int vel) {
+		this.statVel = vel;;
 	}
 }
 
@@ -38,27 +54,51 @@ public class sub2 {
 		FileReader fr = new FileReader("./INFILE/LOCATION.TXT");
 		BufferedReader br = new BufferedReader(fr);
 		
-		String line="", line2="";
+		String line="", line2="", pre="", pre2="";
 		int i=0, j=0, k=0;
 		while(true) {
 			line = br.readLine();
 			line2 = br.readLine();
 			if(!line.contains("#")) {
+				System.out.println(pre);
+				System.out.println(pre2);
 				break;
 			} else {
-				ArrayList<Bus> busList = new ArrayList<Bus>();
-				String[] str = line.split("#");
-				String[] str2 = line2.split("#");
-				for(i=1; i<str.length; i++) {
-					Bus b = new Bus(str[i]);
-					Bus b2 = new Bus(str2[i]);
-					busList.add(b2);
-					int velocity = b2.getBusLoc() - b.getBusLoc();
-					System.out.println(b2.getBusId()+","+b2.getBusLoc()+", "+velocity);
-				}
+				pre = line;
+				pre2 = line2;
 			}
 		}
+		ArrayList<Bus> busList = new ArrayList<Bus>();
+		String[] str = pre.split("#");
+		String[] str2 = pre2.split("#");
+		for(i=1; i<str.length; i++) {
+			Bus b = new Bus(str[i]);
+			Bus b2 = new Bus(str2[i]);
+			busList.add(b2);
+			int velocity = b2.getBusLoc() - b.getBusLoc();
+			busList.get(j).setBusVel(velocity);
+			j++;
+			System.out.println(b2.getBusId()+","+b2.getBusLoc()+", "+velocity);
+		}
 		br.close();
+		
+		j=0;
+		FileReader fr2 = new FileReader("./INFILE/STATION.TXT");
+		BufferedReader br2 = new BufferedReader(fr2);
+//		FileWriter fw2 = new FileWriter("./OUTFILE/ARRIVAL.TXT");
+		ArrayList<Station> statList = new ArrayList<Station>();
+		while(true) {
+			line = br2.readLine();
+			if(line==null) {
+				break;
+			} else {
+				Station s = new Station(line);				
+				statList.add(s);
+				int velocity = s.getStatVel()*1000/3600;
+				statList.get(j).setStatVel(velocity);
+				j++;
+				System.out.println(s.getStatId()+","+s.getStatLoc()+","+s.getStatVel());
+			}
+		}
 	}
-
 }
