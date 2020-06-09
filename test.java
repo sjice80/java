9afa15bbeb5b5d71175ef7656ee8e69f2cee3917
sjice80 <sjice80@gmail.com>
2020-06-09 11:06:53 +0900

    Add files via upload

--- test.java
+++ test.java
@@ -112,6 +112,65 @@ public class test {
 //		String[] phone_book = {"119", "97674223", "1195524421"};
 //		solution(phone_book);
 	}
+	public static void copyFolder(File source, File destination)
+	{
+	    if (source.isDirectory())
+	    {
+	        if (!destination.exists())
+	        {
+	            destination.mkdirs();
+	        }
+
+	        String files[] = source.list();
+
+	        for (String file : files)
+	        {
+	            File srcFile = new File(source, file);
+	            File destFile = new File(destination, file);
+
+	            copyFolder(srcFile, destFile);
+	        }
+	    }
+	    else
+	    {
+	        InputStream in = null;
+	        OutputStream out = null;
+
+	        try
+	        {
+	            in = new FileInputStream(source);
+	            out = new FileOutputStream(destination);
+
+	            byte[] buffer = new byte[1024];
+
+	            int length;
+	            while ((length = in.read(buffer)) > 0)
+	            {
+	                out.write(buffer, 0, length);
+	            }
+	        }
+	        catch (Exception e)
+	        {
+	            try
+	            {
+	                in.close();
+	            }
+	            catch (IOException e1)
+	            {
+	                e1.printStackTrace();
+	            }
+
+	            try
+	            {
+	                out.close();
+	            }
+	            catch (IOException e1)
+	            {
+	                e1.printStackTrace();
+	            }
+	        }
+	    }
+	}
 	public static boolean solution(String[] phone_book) throws IOException {
         boolean answer = true;
         String input_path = "./input/";
